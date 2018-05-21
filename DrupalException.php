9c5c9c03878236f27883635d4a8ac19a7411e340
posterx <info@star-tup.ru>
2018-05-21 13:04:15 +0300

    added DrupalException

---
+++ DrupalException.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace DrupalException;
+
+class DrupalException extends Exception{
+  public $_data;
+  
+  public function __construct($message = "", $code = 0, $data = null, $previous = null){
+    $this->setData($data);
+    parent::__construct($message, $code, $previous);
+  }
+  
+  public function setData($data){
+    $this->_data = $data;
+  }
+  
+  public function getData(){
+    return $this->_data;
+  }
+}
+
+
